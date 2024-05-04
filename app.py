d6286da5b1a81046501e4abed273c5f0ddd9155b
imran0347 <104224003+imran0347@users.noreply.github.com>
2024-05-04 18:00:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def predict():
 
 # python main
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8000, threaded=True, debug=True)
